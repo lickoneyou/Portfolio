66d2b544d1dbff54325cc2d02588ae2ea6fdb416
KIRYL KHOMCHANKA <dickdurex265@gmail.com>
2023-10-04 11:58:41 +0300

    completed header

--- portfolio/src/App.js
+++ portfolio/src/App.js
@@ -1,9 +1,14 @@
-import './App.css';
+import { useSelector } from "react-redux";
 import "./reset.css";
+import "./App.css";
+import Header from "./Components/Header/Header";
 
 function App() {
+  const language = useSelector(state => state)
+
   return (
     <div className="App">
+      <Header language={language}/>
     </div>
   );
 }

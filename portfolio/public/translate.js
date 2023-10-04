3b754fc314f0ca196b53103a6be5ee379035ce02
KIRYL KHOMCHANKA <dickdurex265@gmail.com>
2023-10-04 09:51:49 +0300

    add redux store

--- portfolio/public/translate.js
+++ portfolio/public/translate.js
@@ -1,4 +1,4 @@
-const i18Obj = {
+const translate = {
   'en': {
     'skills': 'Skills',
     'portfolio': 'Portfolio',
@@ -80,4 +80,4 @@ const i18Obj = {
     'send-message': 'Отправить'
   }
 }
-export default i18Obj;
+export default translate;
--- portfolio/src/index.js
+++ portfolio/src/index.js
@@ -1,11 +1,29 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "./index.css";
+import App from "./App";
+import translate from "../public/translate";
+import { configureStore } from "@reduxjs/toolkit";
+import { Provider } from "react-redux";
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+const reducer = (state = translate.en, action) => {
+  switch (action.type) {
+    case "en":
+      return { ...translate.en };
+    case "ru":
+      return { ...translate.ru };
+    default:
+      return state;
+  }
+};
+
+const store = configureStore({ reducer });
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+  <Provider store={store}>
+    <React.StrictMode>
+      <App />
+    </React.StrictMode>
+  </Provider>
 );
